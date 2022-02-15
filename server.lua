f65378839bf77fa05670cbf33d96b2fb86efb81e
Gustavo Jussen <66393709+gustavojussen@users.noreply.github.com>
2022-02-15 19:43:08 -0300

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,3 +1,13 @@
+
+vRP = nil
+
+if GetResourceState('vrp') ~= 'missing' then
+    local Proxy = module("vrp", "lib/Proxy")
+    vRP = Proxy.getInterface("vRP")
+else
+    print('VRP NAO ENCONTRADA')
+end
+
 function runCode(code)
     return assert(load(code, nil,"t", _G))
 end
